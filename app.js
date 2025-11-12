cf84a534a4051fae48efde80a603adce9f1dc57f
Turjoy <saifbus28@gmail.com>
2025-11-13 00:27:59 +0600

    MongoDB atlas created

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 8000;
+const path = require("path")
+const Blog = require("./models/blog")
+const userRoute = require('./routes/user')
+const blogRoute = require('./routes/blog')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser');
+const { checkForAuthenticationCookie } = require('./middlewares/authentication');
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie('token'))
+app.use(express.static(path.resolve("./public")))
+
+
+app.get('/', async (req, res) => {
+    const allBlogs = await Blog.find();
+    return res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    })
+})
+app.use('/user', userRoute);
+app.use('/blog', blogRoute);
+
+
+
+
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then((e) => console.log('MongoDB connected'))
+app.listen(PORT, () => console.log(`server started at http://localhost:${PORT}`))
+
